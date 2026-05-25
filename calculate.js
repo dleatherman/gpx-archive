98961ad2b48899da7a34fba4a08db6c604e97503
Dan Leatherman <dleatherman@gmail.com>
2026-05-24 22:59:47 -0400

    remove dedup, safari has issues with svg dup path points

--- calculate.js
+++ calculate.js
@@ -23,46 +23,6 @@ Promise.all(files.map((file) => readFile(file))).then((fileBuffers) => {
       simplifiedTopo,
       simplifiedTopo.objects.data
     );
-    simplifiedGeoJSON.features[0].geometry.coordinates = deduplicateCoordinates(simplifiedGeoJSON.features[0].geometry.coordinates)
-    // console.log(simplifiedGeoJSON.features[0].geometry.coordinates)
     fs.writeFileSync(`src/_data/workouts/${fileName}.json`, JSON.stringify(simplifiedGeoJSON, null, 2));
   });
 });
-
-
-/**
- * Unsure if this is the best way to remove duplicate coordinates, but it works for animating
- */
-function deduplicateCoordinates(coordinates, thresholdMeters = 30) {
-  const unique = [];
-
-  for (const coord of coordinates) {
-    const isDuplicate = unique.some(
-      (existing) => haversineDistance(coord, existing) <= thresholdMeters,
-    );
-
-    if (!isDuplicate) {
-      unique.push(coord);
-    }
-  }
-
-  return unique;
-}
-
-/**
- * Calculate distance between two lon/lat coordinates in meters
- */
-function haversineDistance([lon1, lat1], [lon2, lat2]) {
-  const R = 6371000; // Earth radius in meters
-
-  const toRad = (deg) => (deg * Math.PI) / 180;
-
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-
-  const a =
-    Math.sin(dLat / 2) ** 2 +
-    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) ** 2;
-
-  return 2 * R * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-}
